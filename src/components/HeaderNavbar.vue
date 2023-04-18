f6b5007a2752c0e995c6a1db7360de5afc501889
DDV2412 <dhyanputra24@gmail.com>
2023-04-18 13:40:59 +0700

    Update Navbar

--- src/components/HeaderNavbar.vue
+++ src/components/HeaderNavbar.vue
@@ -108,7 +108,7 @@
               </div>
               <div v-for="(group, index) in articles" :key="index">
                 <div v-for="(article, idx) in group" :key="idx" class="py-2">
-                  <nuxt-link :to="'/article/' + article.id">
+                  <nuxt-link :to="'/article/' + article.sourceAsMap.doi">
                     <span
                       class="text-l-14 leading-l-14 font-light line-clamp-1 hover:text-ipmugo-primary-50"
                       >{{ article.sourceAsMap.title }}</span
